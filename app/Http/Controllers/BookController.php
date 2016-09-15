51f0352b1ae04b830cb65a4260d9215872bb6757
FelipeMoura <felipe.rosa@dotgroup.com.br>
2016-09-15 20:20:43 -0300

    Refactoring de BookController

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,18 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Book;
-
-use App\Http\Requests;
+use App\Exceptions\SortingServiceException;
+use App\Http\Services\ServiceBookUrlParser;
 
 class BookController extends Controller
 {
-    //
-    public function booksort(Request $request){
-    	return response()->json(Book::with(
-    		['author' => function($query){
-    	    		$query->select(['id','name']);
-    	    }])->get());   
+
+    public function listBooks($params = null){
+        $serviceBook = new ServiceBookUrlParser();
+
+    	try {
+            $params = $serviceBook->urlRulesParser($params);
+            //caso 4
+            if(is_null($params)) throw new SortingServiceException();
+            //caso 5
+            if(empty($params)) return [];
+            //por padrão (do Laravel) isso ira retornar em json e com status code 200
+    		return (new Book())->sortingBooks($params);
+
+    	} catch(SortingServiceException $e){
+    	    //response()->json porque preciso passar um status code 404
+    		return response()->json([ 'message' => $e->errorMessage()],404);
+    	}
     }
 }
