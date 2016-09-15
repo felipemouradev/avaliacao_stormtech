778dfc935dccb987c71c3311f5558cdf0712d878
FelipeMoura <felipe.rosa@dotgroup.com.br>
2016-09-15 20:20:03 -0300

    Metodos de construção do query builder e ordenção

--- app/Book.php
+++ app/Book.php
@@ -6,8 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-	public function author()
-	{
-		return $this->belongsTo('App\Author');
+    public static $fields = [
+        'title','edition','author'
+    ];
+
+    /***
+     * @desc retorna uma consulta base para receber mais parametros
+     */
+	public function bookQuery(){
+	    return $this::join('authors','books.author_id','authors.id')
+            ->select('books.id','books.title','books.year_edition as edition','authors.name as author');
+	}
+
+    /***
+     * @desc Ordena os livros de acordo com os parametros passados e na ordem em que foram passados
+     */
+	public function sortingBooks($params){
+		$builder = $this->bookQuery();
+        foreach ($params as $rule) {
+            $builder->orderBy($rule['field'],$rule['order']);
+        }
+	    return $builder->get();
 	}
 }
